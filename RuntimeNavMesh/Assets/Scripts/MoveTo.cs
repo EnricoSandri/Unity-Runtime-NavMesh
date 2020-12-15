e758f5e4ebab9b61f4fa3afd6f9bf90557847300
Unknown <sandri.enrico86@gmail.com>
2020-12-15 16:21:16 +0000

    Added project to Repo main

---
+++ RuntimeNavMesh/Assets/Scripts/MoveTo.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class MoveTo : MonoBehaviour
+{
+
+    public Transform goal;
+
+    void Start()
+    {
+        NavMeshAgent agent = GetComponent<NavMeshAgent>();
+        agent.destination = goal.position;
+    }
+}
---
+++ RuntimeNavMesh/Assets/Scripts/NavMeshBuilder.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavMeshBuilder : MonoBehaviour
+{
+    public NavMeshSurface surface;
+
+    private void Start()
+    {
+            surface.BuildNavMesh();   //needs to be invoked to solve the error
+    }
+}
